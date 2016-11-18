2da6e76a75bd9dc838fd25739f8121d328c861a5
anthony <thony_prz@outlook.com>
2016-11-18 16:25:33 +0000

    editor W

---
+++ vb/Diálogos/Windows Forms Dialogs/Dialogs.Designer.vb
@@ -0,0 +1,153 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Dialogs
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtFile = New System.Windows.Forms.TextBox()
+        Me.btnOpen = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.bthSave = New System.Windows.Forms.Button()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.FontDialog1 = New System.Windows.Forms.FontDialog()
+        Me.btnFont = New System.Windows.Forms.Button()
+        Me.ColorDialog1 = New System.Windows.Forms.ColorDialog()
+        Me.btmColor = New System.Windows.Forms.Button()
+        Me.PrintDialog1 = New System.Windows.Forms.PrintDialog()
+        Me.btnPrint = New System.Windows.Forms.Button()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.btnBrowse = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtFile
+        '
+        Me.txtFile.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtFile.Location = New System.Drawing.Point(13, 13)
+        Me.txtFile.Multiline = True
+        Me.txtFile.Name = "txtFile"
+        Me.txtFile.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtFile.Size = New System.Drawing.Size(300, 232)
+        Me.txtFile.TabIndex = 0
+        '
+        'btnOpen
+        '
+        Me.btnOpen.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnOpen.Location = New System.Drawing.Point(349, 13)
+        Me.btnOpen.Name = "btnOpen"
+        Me.btnOpen.Size = New System.Drawing.Size(75, 23)
+        Me.btnOpen.TabIndex = 1
+        Me.btnOpen.Text = "Abrir"
+        Me.btnOpen.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'bthSave
+        '
+        Me.bthSave.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.bthSave.Location = New System.Drawing.Point(349, 43)
+        Me.bthSave.Name = "bthSave"
+        Me.bthSave.Size = New System.Drawing.Size(75, 23)
+        Me.bthSave.TabIndex = 2
+        Me.bthSave.Text = "Guardar"
+        Me.bthSave.UseVisualStyleBackColor = True
+        '
+        'btnFont
+        '
+        Me.btnFont.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnFont.Location = New System.Drawing.Point(349, 72)
+        Me.btnFont.Name = "btnFont"
+        Me.btnFont.Size = New System.Drawing.Size(75, 23)
+        Me.btnFont.TabIndex = 3
+        Me.btnFont.Text = "Fuentes"
+        Me.btnFont.UseVisualStyleBackColor = True
+        '
+        'btmColor
+        '
+        Me.btmColor.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btmColor.Location = New System.Drawing.Point(349, 101)
+        Me.btmColor.Name = "btmColor"
+        Me.btmColor.Size = New System.Drawing.Size(75, 23)
+        Me.btmColor.TabIndex = 4
+        Me.btmColor.Text = "Color"
+        Me.btmColor.UseVisualStyleBackColor = True
+        '
+        'PrintDialog1
+        '
+        Me.PrintDialog1.UseEXDialog = True
+        '
+        'btnPrint
+        '
+        Me.btnPrint.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnPrint.Location = New System.Drawing.Point(349, 130)
+        Me.btnPrint.Name = "btnPrint"
+        Me.btnPrint.Size = New System.Drawing.Size(75, 23)
+        Me.btnPrint.TabIndex = 5
+        Me.btnPrint.Text = "Imprimir"
+        Me.btnPrint.UseVisualStyleBackColor = True
+        '
+        'btnBrowse
+        '
+        Me.btnBrowse.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnBrowse.Location = New System.Drawing.Point(349, 159)
+        Me.btnBrowse.Name = "btnBrowse"
+        Me.btnBrowse.Size = New System.Drawing.Size(75, 23)
+        Me.btnBrowse.TabIndex = 6
+        Me.btnBrowse.Text = "Carpetas"
+        Me.btnBrowse.UseVisualStyleBackColor = True
+        '
+        'Dialogs
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(444, 261)
+        Me.Controls.Add(Me.btnBrowse)
+        Me.Controls.Add(Me.btnPrint)
+        Me.Controls.Add(Me.btmColor)
+        Me.Controls.Add(Me.btnFont)
+        Me.Controls.Add(Me.bthSave)
+        Me.Controls.Add(Me.btnOpen)
+        Me.Controls.Add(Me.txtFile)
+        Me.Name = "Dialogs"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Diálogos"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtFile As TextBox
+    Friend WithEvents btnOpen As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents bthSave As Button
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents FontDialog1 As FontDialog
+    Friend WithEvents btnFont As Button
+    Friend WithEvents ColorDialog1 As ColorDialog
+    Friend WithEvents btmColor As Button
+    Friend WithEvents PrintDialog1 As PrintDialog
+    Friend WithEvents btnPrint As Button
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents btnBrowse As Button
+End Class
---
+++ vb/Diálogos/Windows Forms Dialogs/Dialogs.vb
@@ -0,0 +1,180 @@
+﻿Imports System.Drawing.Printing
+Public Class Dialogs
+    'Declaración de variables
+    Private strFileName As String
+
+    Private strPrintRecord As String
+
+    Private WithEvents DialogsPrintDocument As PrintDocument
+
+    Private Sub btnOpen_Click(sender As Object,
+    e As EventArgs) Handles btnOpen.Click
+        'Establecer propiedades del diálog OpenFileDialog
+        OpenFileDialog1.Filter = "Documentos de texto 
+            (*.txt)|*.txt|Todos los archivos (*.*)|*.*"
+        OpenFileDialog1.FilterIndex = 1
+        OpenFileDialog1.Title = "Ejemplo de Open File Dialog"
+
+        'Mostrar diálogo OpenFileDialog y verificar que se carga el archivo
+        If OpenFileDialog1.ShowDialog =
+       Windows.Forms.DialogResult.OK Then
+            Try
+                'Guardar la ruta y nombre del archivo seleccionado
+                strFileName = OpenFileDialog1.FileName
+
+                Dim fileContents As String
+                fileContents = My.Computer.FileSystem.ReadAllText(strFileName)
+
+                'Mostrar todo el contenido del archivo de texto
+                txtFile.Text = fileContents
+
+            Catch ex As Exception
+                MessageBox.Show(ex.Message,
+            My.Application.Info.Title,
+            MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        End If
+    End Sub
+
+    Private Sub bthSave_Click(sender As Object, e As EventArgs) Handles bthSave.Click
+        'Establecer propiedades para el diálogo de guardar archivo SaveFileDialog
+        With SaveFileDialog1
+            .DefaultExt = "txt"
+            .FileName = strFileName
+            .Filter = "Documentos de texto (*.txt)|*.txt|Todos los archivos (*.*)|*.*"
+            .FilterIndex = 1
+            .OverwritePrompt = True
+            .Title = "Ejemplo de Save File Dialog"
+
+        End With
+
+        'Mostrar el diálogo de guardar, y si el usuario pulsa el botón de guardar, guardar el archivo
+        If SaveFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+            Try
+                'Guardar en una variable la ruta y el nombre del archivo
+                strFileName = SaveFileDialog1.FileName
+                'Realizar el guardado del archivo
+                My.Computer.FileSystem.WriteAllText(strFileName, txtFile.Text, False)
+            Catch ex As Exception
+
+                MessageBox.Show(ex.Message, My.Application.Info.Title,
+                MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        End If
+
+    End Sub
+
+    Private Sub btnFont_Click(sender As Object,
+        e As EventArgs) Handles btnFont.Click
+
+        'Establecer las propiedades del diálogo de fuentes FontDialog
+        FontDialog1.ShowColor = True
+
+        'Mostrar el Font Dialog, y si el usuario pulsa Aceptar, actualizar la fuente y color de la caja de texto
+        If FontDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+            txtFile.Font = FontDialog1.Font
+            txtFile.ForeColor = FontDialog1.Color
+        End If
+    End Sub
+
+    Private Sub btmColor_Click(sender As Object, e As EventArgs) Handles btmColor.Click
+        'Mostrar el diálogo de color Color Dialog, y si el usuario pulsa Aceptar actualizar el color de fondo del formulario
+
+        If ColorDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+            Me.BackColor = ColorDialog1.Color
+        End If
+
+    End Sub
+
+    Private Sub DialogsPrintDocument_PrintPage(sender As Object,
+        e As PrintPageEventArgs) Handles DialogsPrintDocument.PrintPage
+        'Declaración de variables
+        Dim intCharactersToPrint As Integer
+        Dim intLinesPerPage As Integer
+        Dim strPrintData As String
+        Dim objStringFormat As New StringFormat
+        Dim objPrintFont As New Font("Arial", 10)
+        Dim objPageBoundaries As RectangleF
+        Dim objPrintArea As SizeF
+
+        'Establecer márgenes
+        objPageBoundaries = New RectangleF(e.MarginBounds.Left,
+            e.MarginBounds.Top, e.MarginBounds.Width,
+            e.MarginBounds.Height)
+
+        'Obtener el área de escritura basado en los márgenes y fuente usada
+        objPrintArea = New SizeF(e.MarginBounds.Width,
+            e.MarginBounds.Height - objPrintFont.GetHeight(e.Graphics))
+
+        'Romper palabras entre líneas
+        objStringFormat.Trimming = StringTrimming.Word
+
+        'Obtener el número de caracters a imprimir
+        e.Graphics.MeasureString(strPrintRecord, objPrintFont, objPrintArea,
+            objStringFormat, intCharactersToPrint, intLinesPerPage)
+
+        'Obtener los datos a imprimir
+        strPrintData = strPrintRecord.Substring(0, intCharactersToPrint)
+
+        'Imprimir la página
+        e.Graphics.DrawString(strPrintData, objPrintFont, Brushes.Black,
+         objPageBoundaries, objStringFormat)
+        'Si hay más líneas, imprimir otra página
+        If intCharactersToPrint < strPrintRecord.Length Then
+            'Borrar el texto impreso del registro de impresión
+            strPrintRecord = strPrintRecord.Remove(0, intCharactersToPrint)
+            e.HasMorePages = True
+        Else
+            e.HasMorePages = False
+        End If
+    End Sub
+
+    Private Sub btnPrint_Click(sender As Object,
+    e As EventArgs) Handles btnPrint.Click
+        'Instanciar una nueva instancia de PrintDocument
+        DialogsPrintDocument = New PrintDocument
+
+        'Establecer las propiedasdesdes de PrintDialog
+        With PrintDialog1
+            .AllowCurrentPage = False
+            .AllowPrintToFile = False
+            .AllowSelection = False
+            .AllowSomePages = False
+            .Document = DialogsPrintDocument
+            .PrinterSettings.DefaultPageSettings.Margins.Top = 25
+            .PrinterSettings.DefaultPageSettings.Margins.Bottom = 25
+            .PrinterSettings.DefaultPageSettings.Margins.Left = 25
+            .PrinterSettings.DefaultPageSettings.Margins.Right = 25
+
+        End With
+
+        If PrintDialog1.ShowDialog = DialogResult.OK Then
+            'Establecer la impresora seleccionada
+            DialogsPrintDocument.PrinterSettings =
+            PrintDialog1.PrinterSettings
+
+            'Obtener texto a imprimir
+            strPrintRecord = txtFile.Text
+
+            'Invocar al método de impresión Print de PrintDocument
+            DialogsPrintDocument.Print()
+        End If
+    End Sub
+
+    Private Sub btnBrowse_Click(sender As Object,
+             e As EventArgs) Handles btnBrowse.Click
+        'Establecer propiedades del diálogo de selección de carpeta FolderBrowser
+        With FolderBrowserDialog1
+            .Description = "Seleccione una carpeta"
+            .RootFolder = Environment.SpecialFolder.MyComputer
+            .ShowNewFolderButton = False
+
+        End With
+
+        'Mostrar el diálogo FolderBrowser y si el usuario pulsa Aceptar mostrar la carpeta seleccionada
+        If FolderBrowserDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+            txtFile.Text = FolderBrowserDialog1.SelectedPath
+        End If
+    End Sub
+
+End Class
