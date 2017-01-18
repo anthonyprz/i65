3aa05c32bb7e1f1baae8003aba1c6a5aba74f109
anthony <thony_prz@outlook.com>
2017-01-18 19:18:50 +0000

    conexion a sql server desde vb.net

---
+++ BDAnuncios/BDAnuncios/Form1.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Data
+Imports System.Data.OleDb
+Imports System.Data.SqlClient
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'BDAnunciosDataSet.anuncio' table. You can move, or remove it, as needed.
+        Me.AnuncioTableAdapter.Fill(Me.BDAnunciosDataSet.anuncio)
+
+
+    End Sub
+
+    Private Sub AnuncioBindingNavigatorSaveItem_Click(sender As Object, e As EventArgs) Handles AnuncioBindingNavigatorSaveItem.Click
+        Me.Validate()
+        Me.AnuncioBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.BDAnunciosDataSet)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Form2.Show()
+    End Sub
+End Class
---
+++ BDAnuncios/BDAnuncios/Form2.vb
@@ -0,0 +1,36 @@
+﻿Imports System.Data
+Imports System.Data.OleDb
+Imports System.Data.SqlClient
+Public Class Form2
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ConnectToSql()
+    End Sub
+    ' Visual Basic
+    Public Sub ConnectToSql()
+        Dim conn As New SqlClient.SqlConnection
+        ' TODO: Modify the connection string and include any
+        ' additional required properties for your database.
+        conn.ConnectionString =
+        "integrated security=SSPI;data source=PC24-A311\SQLEXPRESS;" &
+        "persist security info=False;initial catalog=BDAnuncios"
+        Try
+
+            Dim sql As String = "SELECT * FROM anuncio"
+            Dim dataadapter As New SqlDataAdapter(sql, conn)
+            Dim ds As New DataSet()
+            dataadapter.Fill(ds, "anuncio")
+            conn.Open()
+            DataGridView1.DataSource = ds
+            DataGridView1.DataMember = "anuncio"
+            ' Insert code to process data.
+            ' MessageBox.Show("se ha conectado a la base de datos")
+
+
+
+        Catch ex As Exception
+            MessageBox.Show("Failed to connect to data source")
+        Finally
+            conn.Close()
+        End Try
+    End Sub
+End Class
